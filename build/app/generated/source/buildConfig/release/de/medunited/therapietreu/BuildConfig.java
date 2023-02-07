59ea5fd9b21c404fedcdd16291238b8b16940220
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2023-02-07 15:19:10 +0000

    add flutter_icons + app logos + update dependencies + update release version

---
+++ build/app/generated/source/buildConfig/release/de/medunited/therapietreu/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.medunited.therapietreu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "de.medunited.therapietreu";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "1.0.8";
+}
