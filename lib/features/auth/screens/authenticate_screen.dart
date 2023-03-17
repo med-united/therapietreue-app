061f25c2c0e806ad9e63e23d5ad09f313ab4476b
BeatrizRCorreia <beatriz.correia.pt@gmail.com>
2023-03-17 02:13:41 +0000

    update screen that is currently not used with the appBar

--- lib/features/auth/screens/authenticate_screen.dart
+++ lib/features/auth/screens/authenticate_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,11 +35,28 @@ class _AuthenticateScreenState extends ConsumerState<AuthenticateScreen> {
       }
     });
     return ThemeScaffold(
-        navigationBar: ThemeNavigationBar(
-          context,
-          showBackButton: true,
-          title: "",
-          backgroundColor: ThemeColors.lightPrimaryBackgroundColor,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            color: ThemeColors.primaryColor,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Image.asset(
+              'assets/images/logo.png',
+            ),
+          ),
+          bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(4.0),
+              child: Container(
+                color: ThemeColors.secondaryColor,
+                height: 1.0,
+              )),
         ),
         child: SingleChildScrollView(
             child: Authenticate(
